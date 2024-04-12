4678dd83f3b97f1bcbcf8b508be325c4708bd1dc
21120177 <minhhuy14012003@gmail.com>
2024-04-12 16:48:11 +0700

    stateless and stateful

--- components/DisplayInfo.js
+++ components/DisplayInfo.js
@@ -2,73 +2,111 @@ import React from "react";
 import UserInfo from "./AddUserInfo";
 import './DisplayInfo.scss'
 import logo from './../logo.svg';
-class DisplayInfo extends React.Component {
+// class DisplayInfo extends React.Component {
 
-    constructor(props) {
-        console.log('call constructor: 1');
-        super(props);
-        this.state = {
-            isShowListUsers: true
-        }
-    }
-    componentDidMount() {
-        console.log('>>> call me component did mount');
-        setTimeout(() => {
-            document.title = 'Eric & Hoi Dan IT';
-        })
-    }
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('Components did update', this.props, prevProps);
-        if (this.props.listUsers !== prevProps.listUsers) {
-            if (this.props.listUsers.length === 5) {
-                alert('You got 5 users!')
-            }
-        }
-    }
-    handleShowHide() {
-        this.setState({
-            isShowListUsers: !this.state.isShowListUsers
-        })
-    }
+//     constructor(props) {
+//         console.log('call constructor: 1');
+//         super(props);
+//         this.state = {
+//             isShowListUsers: true
+//         }
+//     }
+//     componentDidMount() {
+//         console.log('>>> call me component did mount');
+//         setTimeout(() => {
+//             document.title = 'Eric & Hoi Dan IT';
+//         })
+//     }
+//     componentDidUpdate(prevProps, prevState, snapshot) {
+//         console.log('Components did update', this.props, prevProps);
+//         if (this.props.listUsers !== prevProps.listUsers) {
+//             if (this.props.listUsers.length === 5) {
+//                 alert('You got 5 users!')
+//             }
+//         }
+//     }
+//     handleShowHide() {
+//         this.setState({
+//             isShowListUsers: !this.state.isShowListUsers
+//         })
+//     }
 
-    render() {
-        console.log('call me render');
-        const { listUsers } = this.props;
-        console.log(listUsers)
-        console.table(listUsers)
-        return (
-            <div className='display-info-container'>
-                {/* <img src={logo} /> */}
-                <div>
-                    <span onClick={() => this.handleShowHide()}>{this.state.isShowListUsers === true ? "Hide list Users" : "Show list Users"}</span>
-                </div>
-                {this.state.isShowListUsers &&
-                    <>
-                        {
-                            listUsers.map((user) => {
+//     render() {
+//         console.log('call me render');
+//         const { listUsers } = this.props;
+//         console.log(listUsers)
+//         console.table(listUsers)
+//         return (
+//             <div className='display-info-container'>
+//                 {/* <img src={logo} /> */}
+//                 <div>
+//                     <span onClick={() => this.handleShowHide()}>{this.state.isShowListUsers === true ? "Hide list Users" : "Show list Users"}</span>
+//                 </div>
+//                 {this.state.isShowListUsers &&
+//                     <>
+//                         {
+//                             listUsers.map((user) => {
 
-                                return (
+//                                 return (
 
-                                    <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                                     <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
 
-                                        {/* <div style={{ color: 'yellow', paddingTop: '50px' }}> My name 's {user.name}</div> */}
-                                        <div > My name 's {user.name}</div>
+//                                         {/* <div style={{ color: 'yellow', paddingTop: '50px' }}> My name 's {user.name}</div> */}
+//                                         <div > My name 's {user.name}</div>
 
-                                        <div> My age 's {user.age}</div>
-                                        <div>
-                                            <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-                                        </div>
+//                                         <div> My age 's {user.age}</div>
+//                                         <div>
+//                                             <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
+//                                         </div>
+//                                     </div>
+
+
+//                                 )
+//                             })
+//                         }
+//                     </>
+//                 }
+//             </div>
+//         )
+//     }
+// }
+
+//stateless and statefull
+const DisplayInfo = (props) => {
+    const { listUsers } = props;
+    return (
+        <div className='display-info-container'>
+            {/* <img src={logo} /> */}
+            {/* <div>
+                <span onClick={() => this.handleShowHide()}>{this.state.isShowListUsers === true ? "Hide list Users" : "Show list Users"}</span>
+            </div> */}
+            {true &&
+                <>
+                    {
+                        listUsers.map((user) => {
+
+                            return (
+
+                                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+
+                                    {/* <div style={{ color: 'yellow', paddingTop: '50px' }}> My name 's {user.name}</div> */}
+                                    <div > My name 's {user.name}</div>
+
+                                    <div> My age 's {user.age}</div>
+                                    <div>
+                                        <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
                                     </div>
+                                </div>
 
 
-                                )
-                            })
-                        }
-                    </>
-                }
-            </div>
-        )
-    }
+                            )
+                        })
+                    }
+                </>
+            }
+        </div>
+    )
 }
+
 
 export default DisplayInfo
