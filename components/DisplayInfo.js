e5d9cbdfc91a46bed15a1f45e9e17705ea013416
21120177 <minhhuy14012003@gmail.com>
2024-04-12 17:01:51 +0700

    use state hook

--- components/DisplayInfo.js
+++ components/DisplayInfo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import UserInfo from "./AddUserInfo";
 import './DisplayInfo.scss'
 import logo from './../logo.svg';
@@ -74,13 +74,22 @@ import logo from './../logo.svg';
 //stateless and statefull
 const DisplayInfo = (props) => {
     const { listUsers } = props;
+    const [isShowListUser, setShowListUser] = useState(true);//khai báo là const để đảm bảo giá trị đc thay đổi thông qua hàm setShowListUser   
+    //destructoring assignment
+    // this.state={
+    //     isShowListUser:true
+    // }
+    const handleShowHideUser = () => {
+
+        setShowListUser(!isShowListUser)
+    }
     return (
         <div className='display-info-container'>
-            {/* <img src={logo} /> */}
-            {/* <div>
-                <span onClick={() => this.handleShowHide()}>{this.state.isShowListUsers === true ? "Hide list Users" : "Show list Users"}</span>
-            </div> */}
-            {true &&
+
+            <div>
+                <span onClick={() => handleShowHideUser()}>{isShowListUser === true ? 'Hide List User' : 'Show List User'}</span>
+            </div>
+            {isShowListUser &&
                 <>
                     {
                         listUsers.map((user) => {
